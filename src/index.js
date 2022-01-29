513f12b87f19992f7dcb00e44a946075f24362e4
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-01-29 21:00:03 +0530

    Add project

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App subject="Ashutosh"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
